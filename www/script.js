7ef445ae5e9e20d993f1811bfa028b12e4907ea9
Jakob Douglas <jakobdouglas1@gmail.com>
2024-08-02 12:53:25 +0930

    refactor: swapped POST method on html object to ajax function

---
+++ www/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+	console.log('Page Loaded');
+	$('#loginform').submit(function (event) {
+		event.preventDefault();
+		ajaxPost();
+	});
+
+	function ajaxPost() {
+		const formdata = {
+			email: $('#email').val(),
+			upwd: $('#upwd').val(),
+		};
+		$.ajax({
+			type: 'POST',
+			contentType: 'application/json',
+			url: 'login',
+			data: JSON.stringify(formdata),
+			dataType: 'json',
+			success: function (results) {
+				console.log(results);
+				if (results.valid == 'true') {
+					$('#loginform').removeClass('fail');
+					$('#loginform').addClass('success');
+				} else {
+					$('#loginform').removeClass('success');
+					$('#loginform').addClass('fail');
+				}
+				$('#postResultsDiv').html(`${results.valid}`);
+			},
+			error: function (e) {
+				alert('error!');
+				console.log('object,', e);
+			},
+		});
+		resetData();
+	}
+	function resetData() {
+		$('#email').val('');
+		$('#upwd').val('');
+	}
+});
